1eab2e14312ffa8ca8e3f2e8ab409827f94912f7
jaijha1995 <jaijhavats95@gmail.com>
2025-11-10 14:24:44 +0530

    New Modify

--- create_job/models.py
+++ create_job/models.py
@@ -84,7 +84,7 @@ class Job_types(TimeStampedModel):
 
 
 class add_job(TimeStampedModel):
-    title = models.CharField(max_length=255,  unique=True,db_index=True)
+    title = models.CharField(max_length=255, db_index=True)
     job_id = models.CharField(max_length=20, unique=True, db_index=True, editable=False)
     Description = models.TextField()
     Salary_range = models.CharField(max_length=100, db_index=True)
