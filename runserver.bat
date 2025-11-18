01fae3ed16d8345e84c700f6edb1d5ec1c1d6fe4
jaijha1995 <jaijhavats95@gmail.com>
2025-11-18 17:09:50 +0530

    New Moify in Runserver.bat

--- runserver.bat
+++ runserver.bat
@@ -4,4 +4,4 @@ python manage.py runserver 127.0.0.1:8080
 
 celery -A restserver worker -l info -c 10 
 
-uvicorn restserver.asgi:application --host 0.0.0.0 --port 8004 --reload
+uvicorn restserver.asgi:application --host 0.0.0.0 --port 8005 --reload
